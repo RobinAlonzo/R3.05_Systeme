77de2f1adff94206c5d12b1a9d1a6a83fbfd9e03
Robin Alonzo <robin.alonzo03@gmail.com>
2022-10-19 20:07:30 +0200

    Fin du TP1

---
+++ TP1/exercice3_4.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void)
+{
+    pid_t pidFork;
+    pidFork = fork();
+    int etat;
+
+    switch (pidFork)
+    {
+    case -1:
+        printf("Creation du fils à échoué \n");
+        exit(EXIT_FAILURE);
+        break;
+
+    case 0:
+        //execlp("ps","ps", (char *) NULL); Recherche dans tous le repertoire la commande
+        //execl("/bin/ps","ps", (char *) NULL); Recherche dans le repertoire donné
+        exit(EXIT_SUCCESS);
+        break;       
+           
+    default:
+        wait(&etat);
+        if (WEXITSTATUS(etat) == EXIT_SUCCESS)
+        {
+            printf("Le processus c'est terminé sans erreur. Exit code: %d\n", WEXITSTATUS(etat));
+        }
+        else
+        {
+            printf("Le processus c'est terminé avec une erreur. Exit code: %d\n", WEXITSTATUS(etat));
+        }
+        exit(EXIT_SUCCESS);
+        break;
+    }
+
+    return 0;
+}
--- TP1/exercice5.c
+++ TP1/exercice5.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
     pid_t pidFork;
     pidFork = fork();
@@ -17,13 +18,20 @@ int main(void)
         break;
 
     case 0:
-        execl("unix","ps", (char *) NULL);
+        execlp(argv[1], argv[1], argv[2], (char *)NULL);
         exit(EXIT_SUCCESS);
         break;       
            
     default:
         wait(&etat);
-        printf("Exit code: %d\n", WEXITSTATUS(etat));
+        if (WEXITSTATUS(etat) == EXIT_SUCCESS)
+        {
+            printf("Le processus c'est terminé sans erreur. Exit code: %d\n", WEXITSTATUS(etat));
+        }
+        else
+        {
+            printf("Le processus c'est terminé avec une erreur. Exit code: %d\n", WEXITSTATUS(etat));
+        }
         exit(EXIT_SUCCESS);
         break;
     }
